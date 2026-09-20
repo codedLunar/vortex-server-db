41a840d3c29191bc5bdeb33b63c2d5e224ae128e
codedLunar <thatlunarr_cod3r@protonmail.com>
2026-09-20 09:43:47 -0500

    revert changes, there is no way to see the server owner from the invite

--- update.js
+++ update.js
@@ -36,7 +36,6 @@ async function updateStats() {
                         name: data.guild.name,
                         description: data.guild.description,
                         ownerName: data.inviter.global_name,
-                        memberCount: data.approximate_member_count,
                         icon: data.guild.icon ? `https://cdn.discordapp.com/icons/${data.guild.id}/${data.guild.icon}.png?size=256` : null
                     }
 
