682d92d9d0294317493469a165df0d43740046cf
codedLunar <thatlunarr_cod3r@protonmail.com>
2026-09-20 09:56:53 -0500

    ACTUALLY revert changes

--- update.js
+++ update.js
@@ -35,7 +35,6 @@ async function updateStats() {
                         code: invite,
                         name: data.guild.name,
                         description: data.guild.description,
-                        ownerName: data.inviter.global_name,
                         icon: data.guild.icon ? `https://cdn.discordapp.com/icons/${data.guild.id}/${data.guild.icon}.png?size=256` : null
                     }
 
