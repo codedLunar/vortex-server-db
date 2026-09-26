527505d3088f23baf68468ea546c06964686db36
codedLunar <thatlunarr_cod3r@protonmail.com>
2026-09-25 20:56:04 -0500

    add logger

---
+++ log.js
@@ -0,0 +1,26 @@
+// node update script
+
+import fs from 'fs/promises';
+
+async function logDatabase() {
+    try {
+        // get the invites data (such a great variable name i know)
+        const databaseData = await fs.readFile('database.json', 'utf8')
+        let db = JSON.parse(databaseData)
+
+        let newObject = {
+            "timestamp": new Date().toISOString(),
+            "data": databaseData
+        }
+
+        db.push(newObject)
+
+        await fs.writeFile('database.json', JSON.stringify(db, null, 2));
+        console.log("(4/4) i successfully did my job now i can rest yaya")
+    } catch (error) {
+        console.error("very bad error, me (Mr update bot) could not log the servers. heres why: ", error)
+        process.exit(1)
+    }
+}
+
+logDatabase();
